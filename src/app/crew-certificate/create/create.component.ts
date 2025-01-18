a1a75f557a9fddb99ddd726ed461648ad3393541
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-18 23:03:11 +0300

    Empty crew certificate create component created.

---
+++ src/app/crew-certificate/create/create.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew-certificate-create',
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.scss',
+})
+export class CreateComponent {}
