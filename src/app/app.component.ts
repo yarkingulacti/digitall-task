542b1513e92d6e39366cf712e164dcb57d955d6a
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-18 23:36:56 +0300

    Layout declarations added to app component view.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
