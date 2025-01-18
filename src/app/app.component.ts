07da32fa02c3df523a8a55a9bdf9ad79c39f721b
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 00:16:53 +0300

    Angular material icon module imported to app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, FooterComponent],
+  imports: [RouterOutlet, MatIconModule, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
