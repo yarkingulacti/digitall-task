4f5698e1edd34daebbec56b8e0cb827ecc07b895
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-18 23:22:32 +0300

    Empty crew edit component created.

---
+++ src/app/crew/edit/edit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew-edit',
+  templateUrl: './edit.component.html',
+  styleUrl: './edit.component.scss',
+})
+export class EditComponent {}
