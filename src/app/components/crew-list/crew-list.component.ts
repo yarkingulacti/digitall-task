14b11d2fdc673ef7103582f58303c755dfbbbc3e
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 00:25:23 +0300

    Crew list table pagination added.

--- src/app/components/crew-list/crew-list.component.ts
+++ src/app/components/crew-list/crew-list.component.ts
@@ -1,16 +1,26 @@
-import { Component, computed, signal } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  computed,
+  signal,
+  ViewChild,
+} from '@angular/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { CrewMember } from '../../../crew';
 import { CrewServiceService } from '../../crew-service.service';
-import { MatTableModule } from '@angular/material/table';
 
 @Component({
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatTableModule, MatPaginatorModule],
   selector: 'component-crew-list',
   templateUrl: './crew-list.component.html',
   styleUrl: './crew-list.component.scss',
 })
-export class CrewListComponent {
-  public dataSource = signal<CrewMember[]>([]);
+export class CrewListComponent implements AfterViewInit {
+  public dataSource = new MatTableDataSource<CrewMember>([]);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+
   public displayedColumns: string[] = [
     'first_name',
     'last_name',
@@ -27,11 +37,16 @@ export class CrewListComponent {
       style: 'currency',
       currency: 'USD',
     }).format(
-      this.dataSource().reduce((acc, crew) => acc + crew.total_income, 0)
+      this.dataSource.data.reduce((acc, crew) => acc + crew.total_income, 0)
     )
   );
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
   constructor(private crewService: CrewServiceService) {
-    this.dataSource.set(this.crewService.crewMembers);
+    this.dataSource.data = this.crewService.crewMembers;
+    this.dataSource.paginator = this.paginator;
   }
 }
