d94dfb26b5f5b20fe0e9f2ba38a599663f1ee03e
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-18 23:03:16 +0300

    Empty crew certificate detail component created.

---
+++ src/app/crew-certificate/detail/detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew-certificate-detail',
+  templateUrl: './detail.component.html',
+  styleUrl: './detail.component.scss',
+})
+export class DetailComponent {}
