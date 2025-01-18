afd26a9f92045a526e1b027c14f005b62988c01a
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-18 22:51:43 +0300

    Empty crew list component created.

---
+++ src/app/components/crew-list/crew-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component-crew-list',
+  templateUrl: './crew-list.component.html',
+  styleUrl: './crew-list.component.scss',
+})
+export class CrewListComponent {}
