14b3b8eb1a4e728b6684d5894efd72a319d3eade
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-18 23:03:24 +0300

    Empty crew certificate list component created.

---
+++ src/app/crew-certificate/list/list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew-certificate-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+})
+export class ListComponent {}
