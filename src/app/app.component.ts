c1aa21046944deef382899112bb16845749ef59c
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-18 22:17:27 +0300

    defaults removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,5 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'digitall-task';
-}
+export class AppComponent {}
