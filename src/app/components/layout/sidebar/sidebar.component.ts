72bbd61e1207954ca2c26027b622f25827cf34c4
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-20 02:30:48 +0300

    sidebar icons added. sidebar layout gaps added.

--- src/app/components/layout/sidebar/sidebar.component.ts
+++ src/app/components/layout/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  imports: [RouterLink, RouterLinkActive, MatListModule],
+  imports: [RouterLink, RouterLinkActive, MatListModule, MatIconModule],
   selector: 'layout-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
