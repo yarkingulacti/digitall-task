b42e431ce31e665947565b3653724bdbc9adceab
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 21:17:54 +0300

    Crew detail views created.

--- src/app/crew/detail/detail.component.ts
+++ src/app/crew/detail/detail.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CrewMember } from '../../../crew';
+import { CrewServiceService } from '../../crew-service.service';
 
 @Component({
+  imports: [CommonModule, MatDividerModule, MatTabsModule],
   selector: 'app-crew-detail',
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss',
+  styleUrls: ['./detail.component.scss'],
 })
-export class CrewDetailComponent {}
+export class CrewDetailComponent implements OnInit {
+  crewMember: CrewMember | undefined;
+
+  constructor(
+    private crewService: CrewServiceService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    const slug = this.route.snapshot.paramMap.get('slug');
+    if (slug) {
+      this.crewMember = this.crewService.getCrew(slug);
+    }
+  }
+
+  isExpired(date: Date): boolean {
+    return new Date(date) < new Date();
+  }
+}
