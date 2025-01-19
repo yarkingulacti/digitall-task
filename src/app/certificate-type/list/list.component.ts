4e545fcd6ffeac65c6e7a36f7ebaf2d7384f6f05
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 21:37:57 +0300

    certificate list component view content added.

--- src/app/certificate-type/list/list.component.ts
+++ src/app/certificate-type/list/list.component.ts
@@ -1,8 +1,59 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CertificateType } from '../../../crew';
+import { certificateTypes } from '../../../certificate-types-data';
+import Swal from 'sweetalert2';
 
 @Component({
+  imports: [
+    RouterOutlet,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIcon,
+  ],
   selector: 'app-certificate-type-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss',
+  styleUrls: ['./list.component.scss'],
 })
-export class CertificateTypeListComponent {}
+export class CertificateTypeListComponent implements AfterViewInit {
+  public dataSource = new MatTableDataSource<CertificateType>([]);
+  public displayedColumns: string[] = ['title', 'description', 'actions'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+
+  constructor(private router: Router) {
+    this.dataSource.data = certificateTypes;
+  }
+
+  ngAfterViewInit() {
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  editCertificateType(certificateType: CertificateType) {
+    this.router.navigate(['/certificate-type', certificateType.id, 'edit']);
+  }
+
+  deleteCertificateType(certificateType: CertificateType) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Implement delete functionality
+        Swal.fire('Deleted!', 'Certificate type has been deleted.', 'success');
+      }
+    });
+  }
+}
