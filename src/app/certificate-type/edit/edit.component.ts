c98b1fa3352593600d226f4f9d72688ddf6bdc40
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-20 00:00:59 +0300

    Certificate type edit functionality done.

--- src/app/certificate-type/edit/edit.component.ts
+++ src/app/certificate-type/edit/edit.component.ts
@@ -1,8 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import {
+  CertificateTypeService,
+  CertificateType,
+} from '../../certificate-type-service.service';
+import Swal from 'sweetalert2';
 
 @Component({
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    CommonModule,
+    MatDialogModule,
+  ],
   selector: 'app-certificate-type-edit',
   templateUrl: './edit.component.html',
-  styleUrl: './edit.component.scss',
+  styleUrls: ['./edit.component.scss'],
 })
-export class CertificateTypeEditComponent {}
+export class CertificateTypeEditComponent implements OnInit {
+  typeForm: FormGroup;
+  typeId: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private certificateTypeService: CertificateTypeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.typeForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.typeId = this.route.snapshot.params['id'];
+    const certificateType = this.certificateTypeService.getCertificateType(
+      this.typeId
+    );
+
+    if (certificateType) {
+      this.typeForm.patchValue({
+        title: certificateType.title,
+        description: certificateType.description,
+      });
+    } else {
+      this.closeModal();
+    }
+  }
+
+  onSubmit() {
+    if (this.typeForm.valid) {
+      const formValue = this.typeForm.value;
+      const updatedType: CertificateType = {
+        id: this.typeId,
+        ...formValue,
+      };
+
+      const success = this.certificateTypeService.editCertificateType(
+        this.typeId,
+        updatedType
+      );
+      if (success) {
+        this.closeModal();
+      }
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Please fill in all required fields.',
+        timerProgressBar: true,
+        timer: 3000,
+      });
+    }
+  }
+
+  onCancel() {
+    this.closeModal();
+  }
+
+  private closeModal() {
+    this.router.navigate(['certificate-type', { outlets: { modal: null } }]);
+  }
+}
