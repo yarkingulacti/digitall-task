c4255b11d3f726c85cfd4cb6fdf69f7640ab2b4a
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 19:53:27 +0300

    Crew certificate detail component removed as it's not needed.

--- src/app/crew-certificate/detail/detail.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-crew-certificate-detail',
-  templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss',
-})
-export class CrewCertificateDetailComponent {}
