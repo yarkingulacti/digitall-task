7cde267f88210e61a853b802bc9c2671164f082d
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 19:13:56 +0300

    Empty certificate type edit component created.

---
+++ src/app/certificate-type/edit/edit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificate-type-edit',
+  templateUrl: './edit.component.html',
+  styleUrl: './edit.component.scss',
+})
+export class CertificateTypeEditComponent {}
