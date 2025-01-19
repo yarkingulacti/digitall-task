13d83e14b0b30c084a8be8534a3ec98b831add3b
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 14:48:12 +0300

    Certificate list component created.

---
+++ src/app/components/certificate-list/certificate-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component-certificate-list',
+  templateUrl: './certificate-list.component.html',
+  styleUrl: './certificate-list.component.scss',
+})
+export class CertificateListComponent {}
