90bd469c550778845b7dbef241137a4ca55674a4
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 19:53:04 +0300

    Crew certificate edit component added.

---
+++ src/app/crew-certificate/edit/edit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew-certificate-edit',
+  templateUrl: './edit.component.html',
+  styleUrl: './edit.component.scss',
+})
+export class CrewCertificateEditComponent {}
