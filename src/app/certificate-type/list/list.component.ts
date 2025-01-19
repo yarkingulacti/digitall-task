349355bb4542614a51cf7a08575bba03fceabaf2
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 19:14:03 +0300

    Empty certificate type list component created.

---
+++ src/app/certificate-type/list/list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificate-type-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+})
+export class CertificateTypeListComponent {}
