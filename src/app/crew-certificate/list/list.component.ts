020e38a298be4e8fe3171c6ed20aa76030d1e059
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 19:53:46 +0300

    Crew certificate modal route outlet added

--- src/app/crew-certificate/list/list.component.ts
+++ src/app/crew-certificate/list/list.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CertificateListComponent } from '../../components/certificate-list/certificate-list.component';
 
 @Component({
+  imports: [RouterOutlet, CertificateListComponent],
   selector: 'app-crew-certificate-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
