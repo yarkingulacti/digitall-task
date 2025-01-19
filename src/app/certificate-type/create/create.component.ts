ff957f8c757f6c3f0572cf725965a0e248beb09c
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-19 19:13:49 +0300

    Empty certificate type create component created.

---
+++ src/app/certificate-type/create/create.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificate-type-create',
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.scss',
+})
+export class CertificateTypeCreateComponent {}
