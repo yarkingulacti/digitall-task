5f35e5b167c3ead7f1283aa6197ebcc8ca53bcf2
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-21 20:10:28 +0300

    Sidebar translations added.

--- src/app/components/layout/sidebar/sidebar.component.ts
+++ src/app/components/layout/sidebar/sidebar.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  imports: [RouterLink, RouterLinkActive, MatListModule, MatIconModule],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatListModule,
+    MatIconModule,
+    TranslateModule,
+  ],
   selector: 'layout-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
